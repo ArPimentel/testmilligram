0f7fe0dc807942d13a55cab12dead61e6596ab08
arturo pimentel <pimentel.arturo@gmail.com>
2020-01-09 10:33:47 +0100

    VmBlockquote

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VmBlockquote texte="Mon text"></VmBlockquote>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VmBlockquote from "./components/VmBlockquote"
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components:{
+    VmBlockquote,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/VmBlockquote.vue
@@ -0,0 +1,18 @@
+<template>
+      <blockquote>
+      <p><em>{{texte}}</em></p>
+    </blockquote>
+</template>
+
+<script>
+export default {
+    name:'VmBlockquote',
+    props:[
+        'texte'
+    ]
+}
+</script>
+
+<style>
+
+</style>
