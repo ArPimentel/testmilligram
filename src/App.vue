4c7efd561b3b1e51aaa9c1d4e3113737e1514d2e
arturo pimentel <pimentel.arturo@gmail.com>
2020-01-09 14:15:23 +0100

    VmHeading

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <VmBlockquote texte="Mon text"></VmBlockquote>
-
+    <VmHeading type="8" texte="Mon titre"></VmHeading>
   </div>
 </template>
 
 <script>
 import VmBlockquote from "./components/VmBlockquote"
+import VmHeading from "./components/VmHeading"
 export default {
   name: 'app',
   components:{
     VmBlockquote,
+    VmHeading
   }
 }
 </script>
---
+++ src/components/VmHeading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <h1 v-if="type === '1'">{{ texte }}</h1> -->
+        
+<!-- 
+    <h2 v-if="type === '2'">{{ texte }}</h2>
+    <h3 v-if="type === '3'">{{ texte }}</h3>
+    <h4 v-if="type === '4'">{{ texte }}</h4>
+    <h5 v-if="type === '5'">{{ texte }}</h5>
+    <h6 v-if="type === '6'">{{ texte }}</h6> -->
+    <component v-if="type <= '6'" :is="elementAAficher">{{ texte }}</component>
+     <button v-else>Autre</button>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VmHeading",
+  props: ["texte", "type"],
+  computed: {
+    elementAAficher() {
+    //   return `h${this.type}`;
+      return "h" + this.type;
+    }
+  }
+};
+</script>
+
+<style></style>
